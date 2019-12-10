3137e3c806c0a83433b5469b2686c194cc51959e
Paul <paul3632356@yahoo.com.tw>
2019-12-10 20:11:45 +0800

    modify 6-4

--- ch6/test6-4.php
+++ ch6/test6-4.php
@@ -3,11 +3,12 @@
   function hello ($x){
       $sum = $x * 2;
       echo "函式執行結束<br />";
+      return $sum;
   }
 ?>
 <p>-------分隔線-------</p>
 <?php
-  hello($x);
+  $sum = hello($x);
   echo "x = $x<br />";
   echo "sum = $sum<br />";
 ?>
