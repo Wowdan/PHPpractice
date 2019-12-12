118569abde600b4af5caae3ddca5be496abb1ec3
Paul <paul3632356@yahoo.com.tw>
2019-12-12 20:01:45 +0800

    modify

--- ch6/test6-12.php
+++ ch6/test6-12.php
@@ -3,5 +3,34 @@ $sum = 0;
 for ($i = 1; $i <= 50; $i++){
     $sum += $i;
 }
-echo "總合式: $sum";
+echo "總合: $sum";
+?>
+
+<?php
+$sum = 0;
+while ( $i <= 50 ){
+    $sum += $i;
+    $i++;
+}
+?>
+
+<?php
+$sum = 0;
+$i = 1;
+do {
+    $sum += $i;
+    $i++;
+}while ($i <= 50);
+?>
+
+<?php
+$sum = 0;
+$i = 0;
+while (true){
+    $i++;
+    $sum += $i;
+    if ($sum >= 100) break;
+}
+echo "\$sum : $sum";
+
 ?>
