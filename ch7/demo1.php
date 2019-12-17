202d7420bd49afc0d472ed3fb7556943f04d3483
Paul <paul3632356@yahoo.com.tw>
2019-12-17 20:36:57 +0800

    modify

--- ch7/demo1.php
+++ ch7/demo1.php
@@ -3,7 +3,6 @@
   $mydog = new Shepherd("peter","棕色","牧羊犬",10);
   echo $mydog->name."<br />";
   echo $mydog->color."<br />";
-  echo $mydog->age."<br />";
   echo $mydog->style."<br />";
   echo $mydog->doWork()."<br />";
   //子類別未改寫父類別的方法
