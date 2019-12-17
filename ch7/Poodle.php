64f24c60d804dfd0743b1f76093eac4b5447d892
Paul <paul3632356@yahoo.com.tw>
2019-12-17 21:14:43 +0800

    modify

--- ch7/Poodle.php
+++ ch7/Poodle.php
@@ -4,7 +4,7 @@ include "dog.php";
 class Poodle extends dog{
     protected $size;
 
-    public function __construct)($name,$color,$style,$size){
+    public function __construct($name,$color,$style,$size){
         parent::__construct($name,$color,$style);
         $this->size = $size;
     }
