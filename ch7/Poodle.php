d70cf29a8c4b8bca366ce877582b9a57270e8e63
Paul <paul3632356@yahoo.com.tw>
2019-12-17 21:27:26 +0800

    modify

--- ch7/Poodle.php
+++ ch7/Poodle.php
@@ -2,17 +2,17 @@
 include "dog.php";
 
 class Poodle extends dog{
-    protected $size;
+  protected $size;
 
-    public function __construct($name,$color,$style,$size){
-        parent::__construct($name,$color,$style);
-        $this->size = $size;
-    }
-    //覆寫狗叫的function
-    public function dogBark(){
-        echo "狗叫...但小聲";
-        //呼叫父親別的dogBark()
-        parent::dogBark(;)
-    }
+  public function __construct($name,$color,$style,$size){
+    parent::__construct($name,$color,$style);
+    $this->size = $size;
+  }
+  // 覆寫狗叫的 function
+  public function dogBark(){
+    echo "狗叫...但小聲....";
+    //呼叫父類別的 dogBark()
+    parent::dogBark();
+  }
 }
 ?>
