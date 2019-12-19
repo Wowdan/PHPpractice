c2ae495f85305374754de7b6e23a3d22a4e45f03
Paul <paul3632356@yahoo.com.tw>
2019-12-19 20:15:39 +0800

    modify

--- ch7/MathRate.php
+++ ch7/MathRate.php
@@ -2,7 +2,7 @@
 class MathRate {
     public static function ComplexRate($principal,$yearRate,$period,$years){
         $result = 0;
-        $result = $principal * pow((1 + ((float)$yearRate/$period)),($period*$years)
+        $result = $principal * pow((1 + ((float)$yearRate/$period)),($period*$years));
         return $result;
     }
 }
