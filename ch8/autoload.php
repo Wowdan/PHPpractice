80cf0c7e66f9ea17cb018c04ba124e0c2cfac667
Paul <paul3632356@yahoo.com.tw>
2019-12-26 21:42:30 +0800

    Namespace

--- ch8/autoload.php
+++ ch8/autoload.php
@@ -1,6 +1,8 @@
 <?php
 function __autoload($className){
+    //從根目錄搜尋，尋找Classes資料夾，副檔名PHP的檔案
     $filename = __DIR__ . "/Classes/" . $className . ".php";
+    //找可讀取的檔案
     if (is_readable($filename)){
         require $filename;
     }
