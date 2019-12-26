446f43aacdcb42c446d235ed5b46dedd9397d5d4
Paul <paul3632356@yahoo.com.tw>
2019-12-26 20:09:34 +0800

    modify

--- ch8/autoload.php
+++ ch8/autoload.php
@@ -1,6 +1,6 @@
 <?php
 function __autoload($className){
-    $filename = __DIR__ . "/classes/" . $className . ".php";
+    $filename = __DIR__ . "/Classes/" . $className . ".php";
     if (is_readable($filename)){
         require $filename;
     }
