d4aa782c24bb43eb55947ab7cf944e974d1564b8
Paul <paul3632356@yahoo.com.tw>
2020-01-07 20:54:07 +0800

    modify

--- ch8/abtractTrait.php
+++ ch8/abtractTrait.php
@@ -19,4 +19,12 @@ class Messenger {
 $messenger = new Messenger;
 $messenger->alert();
 
+function my_error_handler($error_no, $error_msg) {
+    echo "Opps, something went wrong:";
+    echo "Error number: [$error_no]";
+    echo "Error Description: [$error_msg]";
+  }
+  set_error_handler("my_error_handler");
+  echo (5 / 0);
+  
 ?>
