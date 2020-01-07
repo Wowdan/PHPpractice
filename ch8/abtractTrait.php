3a79667dcf0331b53d1aa9d374a1142e12ea5fa5
Paul <paul3632356@yahoo.com.tw>
2020-01-07 20:46:02 +0800

    抽象化Trait，強迫使用。

---
+++ ch8/abtractTrait.php
@@ -0,0 +1,22 @@
+<?php
+trait Message {
+    private $message;
+
+    function alert() {
+        $this->define();
+        echo $this->message;
+    }
+    abstract function define();
+}
+
+class Messenger {
+    use Message;
+    function define() {
+        $this->message = "Custom Message";
+    }
+}
+
+$messenger = new Messenger;
+$messenger->alert();
+
+?>
